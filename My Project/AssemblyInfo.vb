e1719d1c563d75e7fd435e1b8546159cf48c63b0
David-Paul Adams <david-paul.adams@outlook.de>
2025-01-06 10:48:47 +0100

    Projektdateien hinzufügen.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("IT-Nachtsmann")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("BBW Suedhessen")>
+<Assembly: AssemblyProduct("IT-Nachtsmann")>
+<Assembly: AssemblyCopyright("Copyright © BBW Suedhessen 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("73c9c140-cfaf-499b-983a-5b97b7b5fd4f")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
