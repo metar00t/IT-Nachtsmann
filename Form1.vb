94fd8a85aafd197a9658f2abcb9c3bc925f8c963
David-Paul Adams <david-paul.adams@outlook.de>
2025-01-06 15:56:21 +0100

    See TODO on Line 4 in Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -2,11 +2,11 @@
     Dim StrCommand As String
     Private Sub BtnUp_MouseDown(sender As Object, e As MouseEventArgs) Handles BtnUp.MouseDown
         ' TODO: Bugfixing zur Ausführung
-        StrCommand = "sh up_start.sh " + NumUpDownPTZSpeed.Value
+        StrCommand = "sh C:\Users\adamsdav\source\repos\IT-Nachtsmann\scripts\up_start.sh" & NumUpDownPTZSpeed.Value
         Shell("cmd.exe /c " & StrCommand)
     End Sub
     Private Sub BtnUp_MouseUp(sender As Object, e As MouseEventArgs) Handles BtnUp.MouseUp
-        StrCommand = "sh up_stop.sh " + NumUpDownPTZSpeed.Value
+        StrCommand = "sh C:\Users\adamsdav\source\repos\IT-Nachtsmann\scripts\up_stop.sh " & NumUpDownPTZSpeed.Value
         Shell("cmd.exe /c " & StrCommand)
     End Sub
 End Class
