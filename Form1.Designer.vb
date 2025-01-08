ecda6f2fdf5bb7a0f752011a553c9c804f54acdf
David-Paul Adams <david-paul.adams@outlook.de>
2025-01-08 15:13:55 +0100

    First Build ready Version done

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form1
     'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.BtnUp = New System.Windows.Forms.Button()
         Me.BtnRightUp = New System.Windows.Forms.Button()
         Me.BtnLeftUp = New System.Windows.Forms.Button()
@@ -34,13 +35,23 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.NumUpDownPTZSpeed = New System.Windows.Forms.NumericUpDown()
         Me.BtnZoomIn = New System.Windows.Forms.Button()
-        Me.BtnZoomReset = New System.Windows.Forms.Button()
         Me.BtnZoomDecrease = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.NumUpDownPreset = New System.Windows.Forms.NumericUpDown()
         Me.BtnSetPreset = New System.Windows.Forms.Button()
         Me.BtnRemovePreset = New System.Windows.Forms.Button()
-        Me.BtnRunPreset = New System.Windows.Forms.Button()
+        Me.BtnSelectPreset = New System.Windows.Forms.Button()
+        Me.BtnStopLD = New System.Windows.Forms.Button()
+        Me.BtnStopRD = New System.Windows.Forms.Button()
+        Me.BtnStopDown = New System.Windows.Forms.Button()
+        Me.BtnStopLeft = New System.Windows.Forms.Button()
+        Me.BtnStopRight = New System.Windows.Forms.Button()
+        Me.BtnStopLU = New System.Windows.Forms.Button()
+        Me.BtnStopRU = New System.Windows.Forms.Button()
+        Me.BtnStopUp = New System.Windows.Forms.Button()
+        Me.BtnEnd = New System.Windows.Forms.Button()
+        Me.BtnStopZoomIn = New System.Windows.Forms.Button()
+        Me.BtnStopZoomOut = New System.Windows.Forms.Button()
         CType(Me.NumUpDownPTZSpeed, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.NumUpDownPreset, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
@@ -145,36 +156,27 @@ Partial Class Form1
         '
         'BtnZoomIn
         '
-        Me.BtnZoomIn.Location = New System.Drawing.Point(53, 311)
+        Me.BtnZoomIn.Image = CType(resources.GetObject("BtnZoomIn.Image"), System.Drawing.Image)
+        Me.BtnZoomIn.Location = New System.Drawing.Point(53, 280)
         Me.BtnZoomIn.Name = "BtnZoomIn"
-        Me.BtnZoomIn.Size = New System.Drawing.Size(103, 51)
+        Me.BtnZoomIn.Size = New System.Drawing.Size(130, 112)
         Me.BtnZoomIn.TabIndex = 10
-        Me.BtnZoomIn.Text = "+"
         Me.BtnZoomIn.UseVisualStyleBackColor = True
-        '
-        'BtnZoomReset
-        '
-        Me.BtnZoomReset.Location = New System.Drawing.Point(189, 311)
-        Me.BtnZoomReset.Name = "BtnZoomReset"
-        Me.BtnZoomReset.Size = New System.Drawing.Size(103, 51)
-        Me.BtnZoomReset.TabIndex = 11
-        Me.BtnZoomReset.Text = "Reset Zoom"
-        Me.BtnZoomReset.UseVisualStyleBackColor = True
         '
         'BtnZoomDecrease
         '
-        Me.BtnZoomDecrease.Location = New System.Drawing.Point(330, 311)
+        Me.BtnZoomDecrease.Image = CType(resources.GetObject("BtnZoomDecrease.Image"), System.Drawing.Image)
+        Me.BtnZoomDecrease.Location = New System.Drawing.Point(277, 280)
         Me.BtnZoomDecrease.Name = "BtnZoomDecrease"
-        Me.BtnZoomDecrease.Size = New System.Drawing.Size(103, 51)
+        Me.BtnZoomDecrease.Size = New System.Drawing.Size(130, 112)
         Me.BtnZoomDecrease.TabIndex = 12
-        Me.BtnZoomDecrease.Text = "-"
         Me.BtnZoomDecrease.UseVisualStyleBackColor = True
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(66, 418)
+        Me.Label2.Location = New System.Drawing.Point(40, 418)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(63, 20)
         Me.Label2.TabIndex = 13
@@ -182,7 +184,7 @@ Partial Class Form1
         '
         'NumUpDownPreset
         '
-        Me.NumUpDownPreset.Location = New System.Drawing.Point(135, 419)
+        Me.NumUpDownPreset.Location = New System.Drawing.Point(109, 419)
         Me.NumUpDownPreset.Maximum = New Decimal(New Integer() {254, 0, 0, 0})
         Me.NumUpDownPreset.Name = "NumUpDownPreset"
         Me.NumUpDownPreset.Size = New System.Drawing.Size(61, 22)
@@ -190,30 +192,139 @@ Partial Class Form1
         '
         'BtnSetPreset
         '
-        Me.BtnSetPreset.Location = New System.Drawing.Point(218, 419)
+        Me.BtnSetPreset.Location = New System.Drawing.Point(193, 398)
         Me.BtnSetPreset.Name = "BtnSetPreset"
-        Me.BtnSetPreset.Size = New System.Drawing.Size(63, 23)
+        Me.BtnSetPreset.Size = New System.Drawing.Size(78, 28)
         Me.BtnSetPreset.TabIndex = 14
-        Me.BtnSetPreset.Text = "Set"
+        Me.BtnSetPreset.Text = "Set Preset"
         Me.BtnSetPreset.UseVisualStyleBackColor = True
         '
         'BtnRemovePreset
         '
-        Me.BtnRemovePreset.Location = New System.Drawing.Point(287, 419)
+        Me.BtnRemovePreset.Location = New System.Drawing.Point(179, 432)
         Me.BtnRemovePreset.Name = "BtnRemovePreset"
-        Me.BtnRemovePreset.Size = New System.Drawing.Size(63, 23)
+        Me.BtnRemovePreset.Size = New System.Drawing.Size(92, 28)
         Me.BtnRemovePreset.TabIndex = 15
-        Me.BtnRemovePreset.Text = "Delete"
+        Me.BtnRemovePreset.Text = "Clear Preset"
         Me.BtnRemovePreset.UseVisualStyleBackColor = True
         '
-        'BtnRunPreset
+        'BtnSelectPreset
         '
-        Me.BtnRunPreset.Location = New System.Drawing.Point(356, 419)
-        Me.BtnRunPreset.Name = "BtnRunPreset"
-        Me.BtnRunPreset.Size = New System.Drawing.Size(63, 23)
-        Me.BtnRunPreset.TabIndex = 16
-        Me.BtnRunPreset.Text = "Run"
-        Me.BtnRunPreset.UseVisualStyleBackColor = True
+        Me.BtnSelectPreset.Location = New System.Drawing.Point(277, 398)
+        Me.BtnSelectPreset.Name = "BtnSelectPreset"
+        Me.BtnSelectPreset.Size = New System.Drawing.Size(87, 62)
+        Me.BtnSelectPreset.TabIndex = 16
+        Me.BtnSelectPreset.Text = "Select Preset"
+        Me.BtnSelectPreset.UseVisualStyleBackColor = True
+        '
+        'BtnStopLD
+        '
+        Me.BtnStopLD.Location = New System.Drawing.Point(53, 165)
+        Me.BtnStopLD.Name = "BtnStopLD"
+        Me.BtnStopLD.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopLD.TabIndex = 25
+        Me.BtnStopLD.Text = "Stop"
+        Me.BtnStopLD.UseVisualStyleBackColor = True
+        Me.BtnStopLD.Visible = False
+        '
+        'BtnStopRD
+        '
+        Me.BtnStopRD.Location = New System.Drawing.Point(330, 165)
+        Me.BtnStopRD.Name = "BtnStopRD"
+        Me.BtnStopRD.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopRD.TabIndex = 24
+        Me.BtnStopRD.Text = "Stop"
+        Me.BtnStopRD.UseVisualStyleBackColor = True
+        Me.BtnStopRD.Visible = False
+        '
+        'BtnStopDown
+        '
+        Me.BtnStopDown.Location = New System.Drawing.Point(189, 165)
+        Me.BtnStopDown.Name = "BtnStopDown"
+        Me.BtnStopDown.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopDown.TabIndex = 23
+        Me.BtnStopDown.Text = "Stop"
+        Me.BtnStopDown.UseVisualStyleBackColor = True
+        Me.BtnStopDown.Visible = False
+        '
+        'BtnStopLeft
+        '
+        Me.BtnStopLeft.Location = New System.Drawing.Point(53, 101)
+        Me.BtnStopLeft.Name = "BtnStopLeft"
+        Me.BtnStopLeft.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopLeft.TabIndex = 22
+        Me.BtnStopLeft.Text = "Stop"
+        Me.BtnStopLeft.UseVisualStyleBackColor = True
+        Me.BtnStopLeft.Visible = False
+        '
+        'BtnStopRight
+        '
+        Me.BtnStopRight.Location = New System.Drawing.Point(330, 101)
+        Me.BtnStopRight.Name = "BtnStopRight"
+        Me.BtnStopRight.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopRight.TabIndex = 21
+        Me.BtnStopRight.Text = "Stop"
+        Me.BtnStopRight.UseVisualStyleBackColor = True
+        Me.BtnStopRight.Visible = False
+        '
+        'BtnStopLU
+        '
+        Me.BtnStopLU.Location = New System.Drawing.Point(53, 40)
+        Me.BtnStopLU.Name = "BtnStopLU"
+        Me.BtnStopLU.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopLU.TabIndex = 19
+        Me.BtnStopLU.Text = "Stop"
+        Me.BtnStopLU.UseVisualStyleBackColor = True
+        Me.BtnStopLU.Visible = False
+        '
+        'BtnStopRU
+        '
+        Me.BtnStopRU.Location = New System.Drawing.Point(330, 40)
+        Me.BtnStopRU.Name = "BtnStopRU"
+        Me.BtnStopRU.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopRU.TabIndex = 18
+        Me.BtnStopRU.Text = "Stop"
+        Me.BtnStopRU.UseVisualStyleBackColor = True
+        Me.BtnStopRU.Visible = False
+        '
+        'BtnStopUp
+        '
+        Me.BtnStopUp.Location = New System.Drawing.Point(189, 40)
+        Me.BtnStopUp.Name = "BtnStopUp"
+        Me.BtnStopUp.Size = New System.Drawing.Size(103, 50)
+        Me.BtnStopUp.TabIndex = 17
+        Me.BtnStopUp.Text = "Stop"
+        Me.BtnStopUp.UseVisualStyleBackColor = True
+        Me.BtnStopUp.Visible = False
+        '
+        'BtnEnd
+        '
+        Me.BtnEnd.Location = New System.Drawing.Point(370, 398)
+        Me.BtnEnd.Name = "BtnEnd"
+        Me.BtnEnd.Size = New System.Drawing.Size(87, 62)
+        Me.BtnEnd.TabIndex = 26
+        Me.BtnEnd.Text = "Shutdown"
+        Me.BtnEnd.UseVisualStyleBackColor = True
+        '
+        'BtnStopZoomIn
+        '
+        Me.BtnStopZoomIn.Location = New System.Drawing.Point(53, 280)
+        Me.BtnStopZoomIn.Name = "BtnStopZoomIn"
+        Me.BtnStopZoomIn.Size = New System.Drawing.Size(130, 112)
+        Me.BtnStopZoomIn.TabIndex = 27
+        Me.BtnStopZoomIn.Text = "Stop"
+        Me.BtnStopZoomIn.UseVisualStyleBackColor = True
+        Me.BtnStopZoomIn.Visible = False
+        '
+        'BtnStopZoomOut
+        '
+        Me.BtnStopZoomOut.Location = New System.Drawing.Point(277, 280)
+        Me.BtnStopZoomOut.Name = "BtnStopZoomOut"
+        Me.BtnStopZoomOut.Size = New System.Drawing.Size(130, 112)
+        Me.BtnStopZoomOut.TabIndex = 28
+        Me.BtnStopZoomOut.Text = "Stop"
+        Me.BtnStopZoomOut.UseVisualStyleBackColor = True
+        Me.BtnStopZoomOut.Visible = False
         '
         'Form1
         '
@@ -221,13 +332,23 @@ Partial Class Form1
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Control
         Me.ClientSize = New System.Drawing.Size(475, 475)
-        Me.Controls.Add(Me.BtnRunPreset)
+        Me.Controls.Add(Me.BtnStopZoomOut)
+        Me.Controls.Add(Me.BtnStopZoomIn)
+        Me.Controls.Add(Me.BtnEnd)
+        Me.Controls.Add(Me.BtnStopLD)
+        Me.Controls.Add(Me.BtnStopRD)
+        Me.Controls.Add(Me.BtnStopDown)
+        Me.Controls.Add(Me.BtnStopLeft)
+        Me.Controls.Add(Me.BtnStopRight)
+        Me.Controls.Add(Me.BtnStopLU)
+        Me.Controls.Add(Me.BtnStopRU)
+        Me.Controls.Add(Me.BtnStopUp)
+        Me.Controls.Add(Me.BtnSelectPreset)
         Me.Controls.Add(Me.BtnRemovePreset)
         Me.Controls.Add(Me.BtnSetPreset)
         Me.Controls.Add(Me.NumUpDownPreset)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.BtnZoomDecrease)
-        Me.Controls.Add(Me.BtnZoomReset)
         Me.Controls.Add(Me.BtnZoomIn)
         Me.Controls.Add(Me.NumUpDownPTZSpeed)
         Me.Controls.Add(Me.Label1)
@@ -240,8 +361,9 @@ Partial Class Form1
         Me.Controls.Add(Me.BtnLeftUp)
         Me.Controls.Add(Me.BtnRightUp)
         Me.Controls.Add(Me.BtnUp)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
-        Me.Text = "Fernbedienung PTZ"
+        Me.Text = "PTZ Remote Control"
         CType(Me.NumUpDownPTZSpeed, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NumUpDownPreset, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
@@ -261,11 +383,21 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents NumUpDownPTZSpeed As NumericUpDown
     Friend WithEvents BtnZoomIn As Button
-    Friend WithEvents BtnZoomReset As Button
     Friend WithEvents BtnZoomDecrease As Button
     Friend WithEvents Label2 As Label
     Friend WithEvents NumUpDownPreset As NumericUpDown
     Friend WithEvents BtnSetPreset As Button
     Friend WithEvents BtnRemovePreset As Button
-    Friend WithEvents BtnRunPreset As Button
+    Friend WithEvents BtnSelectPreset As Button
+    Friend WithEvents BtnStopLD As Button
+    Friend WithEvents BtnStopRD As Button
+    Friend WithEvents BtnStopDown As Button
+    Friend WithEvents BtnStopLeft As Button
+    Friend WithEvents BtnStopRight As Button
+    Friend WithEvents BtnStopLU As Button
+    Friend WithEvents BtnStopRU As Button
+    Friend WithEvents BtnStopUp As Button
+    Friend WithEvents BtnEnd As Button
+    Friend WithEvents BtnStopZoomIn As Button
+    Friend WithEvents BtnStopZoomOut As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,12 +1,226 @@
-﻿Public Class Form1
-    Dim StrCommand As String
-    Private Sub BtnUp_MouseDown(sender As Object, e As MouseEventArgs) Handles BtnUp.MouseDown
-        ' TODO: Bugfixing zur Ausführung
-        StrCommand = "sh C:\Users\adamsdav\source\repos\IT-Nachtsmann\scripts\up_start.sh" & NumUpDownPTZSpeed.Value
-        Shell("cmd.exe /c " & StrCommand)
+﻿Imports System.IO
+Imports System.Net
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim AskURL As Object
+        Dim Title, Prompt, [Default] As String
+
+        Title = "Establish Connection"
+        Prompt = "Please enter the URL of your Device"
+        [Default] = ""
+
+        AskURL = InputBox(Prompt, Title, [Default])
+        Dim MissingInput = String.IsNullOrEmpty(AskURL)
+
+        If MissingInput Then
+            MessageBox.Show("Please try again", "Error 404", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Close()
+        End If
+
+        DeviceURL = AskURL
     End Sub
-    Private Sub BtnUp_MouseUp(sender As Object, e As MouseEventArgs) Handles BtnUp.MouseUp
-        StrCommand = "sh C:\Users\adamsdav\source\repos\IT-Nachtsmann\scripts\up_stop.sh " & NumUpDownPTZSpeed.Value
-        Shell("cmd.exe /c " & StrCommand)
+    Dim StrCommand As String
+    Dim DeviceURL
+    Public Sub CamControl(URL, CMD)
+        Try
+            Dim myReq As HttpWebRequest
+            Dim myResp As HttpWebResponse
+            Dim myReader As StreamReader
+            myReq = HttpWebRequest.Create(URL)
+            myReq.Method = "POST"
+            myReq.ContentType = "application/x-www-form-urlencoded"
+            myReq.Accept = "application/json"
+            Dim myData As String = "szCmd=%7B%22SysCtrl%22%3A%7B%22PtzCtrl%22%3A%7B%22nChanel%22%3A0%2C%22szPtzCmd%22%3A%22" & CMD & "%22%2C%22byValue%22%3A" & NumUpDownPTZSpeed.Value & "%7D%7D%7D"
+            myReq.GetRequestStream.Write(System.Text.Encoding.UTF8.GetBytes(myData), 0, System.Text.Encoding.UTF8.GetBytes(myData).Count)
+            myResp = myReq.GetResponse
+            myReader = New System.IO.StreamReader(myResp.GetResponseStream)
+            MessageBox.Show(myReader.ReadToEnd, "Success")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub PresetControl(URL, CMD)
+        Try
+            Dim myReq As HttpWebRequest
+            Dim myResp As HttpWebResponse
+            Dim myReader As StreamReader
+            myReq = HttpWebRequest.Create(URL)
+            myReq.Method = "POST"
+            myReq.ContentType = "application/x-www-form-urlencoded"
+            myReq.Accept = "application/json"
+            Dim myData As String = "szCmd=%7B%22SysCtrl%22%3A%7B%22PtzCtrl%22%3A%7B%22nChanel%22%3A0%2C%22szPtzCmd%22%3A%22" & CMD & "%22%2C%22byValue%22%3A" & NumUpDownPreset.Value & "%7D%7D%7D"
+            myReq.GetRequestStream.Write(System.Text.Encoding.UTF8.GetBytes(myData), 0, System.Text.Encoding.UTF8.GetBytes(myData).Count)
+            myResp = myReq.GetResponse
+            myReader = New System.IO.StreamReader(myResp.GetResponseStream)
+            MessageBox.Show(myReader.ReadToEnd, "Success")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    Private Sub BtnUp_Click(sender As Object, e As EventArgs) Handles BtnUp.Click
+        StrCommand = "up_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnUp.Visible = False
+        BtnStopUp.Visible = True
+    End Sub
+    Private Sub BtnStopUp_Click(sender As Object, e As EventArgs) Handles BtnStopUp.Click
+        StrCommand = "up_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopUp.Visible = False
+        BtnUp.Visible = True
+    End Sub
+
+    Private Sub BtnHome_Click(sender As Object, e As EventArgs) Handles BtnHome.Click
+        StrCommand = "go_home"
+        CamControl(DeviceURL, StrCommand)
+    End Sub
+
+    Private Sub BtnLeftUp_Click(sender As Object, e As EventArgs) Handles BtnLeftUp.Click
+        StrCommand = "leftup_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnLeftUp.Visible = False
+        BtnStopLU.Visible = True
+    End Sub
+
+    Private Sub BtnStopLU_Click(sender As Object, e As EventArgs) Handles BtnStopLU.Click
+        StrCommand = "leftup_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopLU.Visible = False
+        BtnLeftUp.Visible = True
+    End Sub
+
+    Private Sub BtnLeft_Click(sender As Object, e As EventArgs) Handles BtnLeft.Click
+        StrCommand = "left_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnLeft.Visible = False
+        BtnStopLeft.Visible = True
+    End Sub
+
+    Private Sub BtnStopLeft_Click(sender As Object, e As EventArgs) Handles BtnStopLeft.Click
+        StrCommand = "left_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopLeft.Visible = False
+        BtnLeft.Visible = True
+    End Sub
+
+    Private Sub BtnLeftDown_Click(sender As Object, e As EventArgs) Handles BtnLeftDown.Click
+        StrCommand = "leftdown_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnLeftDown.Visible = False
+        BtnStopLD.Visible = True
+    End Sub
+
+    Private Sub BtnStopLD_Click(sender As Object, e As EventArgs) Handles BtnStopLD.Click
+        StrCommand = "leftdown_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopLD.Visible = False
+        BtnLeftDown.Visible = True
+    End Sub
+
+    Private Sub BtnDown_Click(sender As Object, e As EventArgs) Handles BtnDown.Click
+        StrCommand = "down_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnDown.Visible = False
+        BtnStopDown.Visible = True
+    End Sub
+
+    Private Sub BtnStopDown_Click(sender As Object, e As EventArgs) Handles BtnStopDown.Click
+        StrCommand = "down_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopDown.Visible = False
+        BtnDown.Visible = True
+    End Sub
+
+    Private Sub BtnRightDown_Click(sender As Object, e As EventArgs) Handles BtnRightDown.Click
+        StrCommand = "rightdown_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnRightDown.Visible = False
+        BtnStopRD.Visible = True
+    End Sub
+
+    Private Sub BtnStopRD_Click(sender As Object, e As EventArgs) Handles BtnStopRD.Click
+        StrCommand = "rightdown_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopRD.Visible = False
+        BtnRightDown.Visible = True
+    End Sub
+
+    Private Sub BtnRight_Click(sender As Object, e As EventArgs) Handles BtnRight.Click
+        StrCommand = "right_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnRight.Visible = False
+        BtnStopRight.Visible = True
+    End Sub
+
+    Private Sub BtnStopRight_Click(sender As Object, e As EventArgs) Handles BtnStopRight.Click
+        StrCommand = "right_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopRight.Visible = False
+        BtnRight.Visible = True
+    End Sub
+
+    Private Sub BtnRightUp_Click(sender As Object, e As EventArgs) Handles BtnRightUp.Click
+        StrCommand = "rightup_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnRightUp.Visible = False
+        BtnStopRU.Visible = True
+    End Sub
+
+    Private Sub BtnStopRU_Click(sender As Object, e As EventArgs) Handles BtnStopRU.Click
+        StrCommand = "rightup_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopRU.Visible = False
+        BtnRightUp.Visible = True
+    End Sub
+
+    Private Sub BtnEnd_Click(sender As Object, e As EventArgs) Handles BtnEnd.Click
+        MessageBox.Show("You have been successfully disconnected", "Program ends now", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Close()
+    End Sub
+
+    Private Sub BtnSetPreset_Click(sender As Object, e As EventArgs) Handles BtnSetPreset.Click
+        StrCommand = "preset_set"
+        PresetControl(DeviceURL, StrCommand)
+        MessageBox.Show("Preset has been successfully saved on Number " & NumUpDownPreset.Value)
+    End Sub
+
+    Private Sub BtnRemovePreset_Click(sender As Object, e As EventArgs) Handles BtnRemovePreset.Click
+        StrCommand = "preset_clean"
+        PresetControl(DeviceURL, StrCommand)
+        MessageBox.Show("Preset #" & NumUpDownPreset.Value & " has been successfully removed")
+    End Sub
+
+    Private Sub BtnSelectPreset_Click(sender As Object, e As EventArgs) Handles BtnSelectPreset.Click
+        StrCommand = "preset_call"
+        CamControl(DeviceURL, StrCommand)
+        MessageBox.Show("Preset #" & NumUpDownPreset.Value & " successfully loaded!")
+    End Sub
+
+    Private Sub BtnZoomIn_Click(sender As Object, e As EventArgs) Handles BtnZoomIn.Click
+        StrCommand = "zoomadd_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnZoomIn.Visible = False
+        BtnStopZoomIn.Visible = True
+    End Sub
+
+    Private Sub BtnStopZoomIn_Click(sender As Object, e As EventArgs) Handles BtnStopZoomIn.Click
+        StrCommand = "zoomadd_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopZoomIn.Visible = False
+        BtnZoomIn.Visible = True
+    End Sub
+
+    Private Sub BtnZoomDecrease_Click(sender As Object, e As EventArgs) Handles BtnZoomDecrease.Click
+        StrCommand = "zoomdec_start"
+        CamControl(DeviceURL, StrCommand)
+        BtnZoomDecrease.Visible = False
+        BtnStopZoomOut.Visible = True
+    End Sub
+
+    Private Sub BtnStopZoomOut_Click(sender As Object, e As EventArgs) Handles BtnStopZoomOut.Click
+        StrCommand = "zoomdec_stop"
+        CamControl(DeviceURL, StrCommand)
+        BtnStopZoomOut.Visible = False
+        BtnZoomDecrease.Visible = True
     End Sub
 End Class
